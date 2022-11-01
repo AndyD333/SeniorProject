daf139401f5fffd8535681d584d036484dfbf84f
prolly25 <36855174+prolly25@users.noreply.github.com>
2022-11-01 15:47:54 -0400

    Update Gate.java

--- SeniorProject/src/seniorproject/Gate.java
+++ SeniorProject/src/seniorproject/Gate.java
@@ -4,46 +4,106 @@ public class Gate implements Source, Destination
 {
     //0 = AND
     //1 = OR
-    //2 = NAND
-    //3 = NOR
-    //4 = XOR
-    private static int typeNumber;
+    //2 = NOR
+    //3 = NAND
+    //4 = NOR
+    //5 = XOR
+    private int typeNumber;
     
-    private static Source sourceL;
-    private static Source sourceR;
+    private Source sourceL;
+    private Source sourceR;
     private boolean powered;
     
     public Gate()
     {
-        typeNumber = 0;
-	  powered = false;
+        this.typeNumber = 0;
+	this.powered = false;
     }
     
     public Gate(int a, Source l, Source r)
     {
-        //SourceL = l;
-        //SourceR = r;
-        typeNumber = a;
-	  powered = false;
+        this.sourceL = l;
+        this.sourceR = r;
+        this.typeNumber = a;
+	this.powered = false;
     }
     
     public int getType()
     {
-        return typeNumber;
+        return this.typeNumber;
     }
     
     public Source getSourceL()
     {
-        return sourceL;
+        return this.sourceL;
     }
     
     public Source getSourceR()
     {
-        return sourceR;
+        return this.sourceR;
+    }
+
+     
+    public void updatePower()
+    {
+  
+        if(this.typeNumber == 0)//OR GATE
+        {
+            if(this.sourceL.getPower() && this.sourceR.getPower())
+            {
+                this.powered = true;
+            }
+        }
+        else if(this.typeNumber == 1)//AND GATE
+        {
+            if(this.sourceL.getPower() && this.sourceR.getPower())
+            {
+                this.powered = true;
+            }
+        }
+        else if(this.typeNumber == 2)//NOT, NAND, NOR, XOR
+        {
+            if(!this.sourceL.getPower())
+            {
+                this.powered = true;
+            }
+        }
+        else if(this.typeNumber == 3)//NAND GATE
+        {
+            if(!this.sourceR.getPower() || !this.sourceL.getPower())
+            {
+                this.powered = true;
+            }
+        }
+        else if(this.typeNumber == 4)
+        {
+            if(!this.sourceR.getPower() && !this.sourceL.getPower())
+            {
+                this.powered = true;
+            }
+        }
+        if(this.sourceR.getPower() != this.sourceL.getPower())
+        {
+            this.powered = true;
+        }
     }
 
     public boolean getPower()
     {
-	return powered;
+        updatePower();
+        if(this.powered)
+        {
+            System.out.println("ON!");
+        }
+        else
+        {
+            System.out.println("OFF!");
+        }
+	return this.powered;
+    }
+
+    @Override
+    public boolean getSourcePower() {
+        return sourceL.getPower();
     }
 }
