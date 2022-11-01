460fc077c972d89d1bd148b57a61f31026ea1076
prolly25 <gerjohnson2@gmail.com>
2022-11-01 12:48:40 -0400

    classtime

--- SeniorProject/src/seniorproject/Gate.java
+++ SeniorProject/src/seniorproject/Gate.java
@@ -4,143 +4,46 @@ public class Gate implements Source, Destination
 {
     //0 = AND
     //1 = OR
-    //2 = NOT
-    //3 = NAND
-    //4 = NOR
-    //5 = XOR
+    //2 = NAND
+    //3 = NOR
+    //4 = XOR
     private static int typeNumber;
     
-    private Source sourceL;
-    private Source sourceR;
+    private static Source sourceL;
+    private static Source sourceR;
     private boolean powered;
     
     public Gate()
     {
-        this.typeNumber = 0;
-	this.powered = false;
+        typeNumber = 0;
+	  powered = false;
     }
     
     public Gate(int a, Source l, Source r)
     {
-        this.sourceL = l;
-        this.sourceR = r;
-        this.typeNumber = a;
-	this.powered = false;
+        //SourceL = l;
+        //SourceR = r;
+        typeNumber = a;
+	  powered = false;
     }
     
     public int getType()
     {
-        return this.typeNumber;
+        return typeNumber;
     }
     
     public Source getSourceL()
     {
-        return this.sourceL;
+        return sourceL;
     }
     
     public Source getSourceR()
     {
         return sourceR;
     }
-    
-    public void updatePower()
-    {
-        switch (this.typeNumber)
-        {
-            //AND GATE
-            case 0:
-                if(this.sourceR.getPower() & this.sourceL.getPower())
-                {
-                    this.powered = true;
-                }
-                else
-                {
-                    this.powered = false;
-                }
-                break;
-                
-            //OR GATE
-            case 1:
-                if(this.sourceR.getPower() | this.sourceL.getPower())
-                {
-                    this.powered = true;
-                }
-                else
-                {
-                    this.powered = false;
-                }
-                break;
-             
-            //NOT GATE
-            case 2:
-                if(this.sourceR.getPower())
-                {
-                    this.powered = false;
-                }
-                else
-                {
-                    this.powered = true;
-                }
-                break;
-                
-            //NAND GATE
-            case 3:
-                if(this.sourceR.getPower() & this.sourceL.getPower())
-                {
-                    this.powered = false;
-                }
-                else
-                {
-                    this.powered = true;
-                }
-                break;
-                
-            //NOR GATE
-            case 4:
-                if(this.sourceR.getPower() | this.sourceL.getPower())
-                {
-                    this.powered = false;
-                }
-                else
-                {
-                    this.powered = true;
-                }
-                break;
-            
-            //XOR GATE
-            case 5:
-                if((this.sourceL.getPower() & !this.sourceR.getPower()) | (this.sourceR.getPower() & !this.sourceL.getPower()))
-                {
-                    this.powered = true;
-                }
-                else
-                {
-                    this.powered = false;
-                }
-                break;
-                
-            default:
-                this.powered = false;
-                break;
-        }
-    }
 
     public boolean getPower()
     {
-        updatePower();
-        if(powered)
-        {
-            System.out.println("ON!");
-        }
-        else
-        {
-            System.out.println("OFF!");
-        }
 	return powered;
-    }
-
-    @Override
-    public boolean getSourcePower() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
--- SeniorProject/src/seniorproject/SeniorProject.java
+++ SeniorProject/src/seniorproject/SeniorProject.java
@@ -3,16 +3,9 @@ package seniorproject;
 public class SeniorProject
 {
     public static void main(String[] args) {
-        Button on = new Button(true);
-        Button off = new Button(false);
+        Button one = new Button();
+        Button two = new Button();
         
-        Gate orA = new Gate(5, on, on);
-        Gate orB = new Gate(5, on, off);
-        Gate orC = new Gate(5, off, on);
-        Gate orD = new Gate(5, off, off);
-        orA.getPower();
-        orB.getPower();
-        orC.getPower();
-        orD.getPower();
+        
     }
 }
