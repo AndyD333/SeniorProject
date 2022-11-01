800aca1ca0f255d5d10158f2e54b3569b2ac9e32
prolly25 <gerjohnson2@gmail.com>
2022-11-01 10:09:53 -0400

    Revert "light object work"
    This reverts commit 3e3fbacf523a305f168f24cc0ba0e0131b09009b.

---
+++ SeniorProject/src/seniorproject/SeniorProject.java
@@ -0,0 +1,11 @@
+package seniorproject;
+
+public class SeniorProject
+{
+    public static void main(String[] args) {
+        Button one = new Button();
+        Button two = new Button();
+        
+        
+    }
+}
