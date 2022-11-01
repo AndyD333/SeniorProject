c45089589fc4d95bd9b1efe50aa51aa9556dccaa
prolly25 <36855174+prolly25@users.noreply.github.com>
2022-11-01 15:47:27 -0400

    Update SeniorProject.java

--- SeniorProject/src/seniorproject/SeniorProject.java
+++ SeniorProject/src/seniorproject/SeniorProject.java
@@ -2,10 +2,26 @@ package seniorproject;
 
 public class SeniorProject
 {
-    public static void main(String[] args) {
-        Button one = new Button();
-        Button two = new Button();
+        public static void main(String[] args)
+        {
+        Button on = new Button(true);
+        Button off = new Button(false);
+
         
         
-    }
+        Gate orA = new Gate(0, on, on);
+        Gate orB = new Gate(0, on, off);
+        Gate orC = new Gate(0, off, on);
+        Gate orD = new Gate(0, off, off);
+        
+        
+        orA.getPower();
+        orB.getPower();
+        orC.getPower();
+        orD.getPower();
+        
+            System.out.println("");
+        Light finish = new Light(orD);
+        finish.getPower();
+        }
 }
