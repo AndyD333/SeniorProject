455be813171705b4b473c3a169184edda7319ebd
prolly25 <36855174+prolly25@users.noreply.github.com>
2022-11-11 11:45:11 -0500

    Update ProjectForJavaRunner.java

--- demo/ProjectForJava/src/projectforjava/ProjectForJavaRunner.java
+++ demo/ProjectForJava/src/projectforjava/ProjectForJavaRunner.java
@@ -1,20 +1,7 @@
-package projectforjava;
-
 public class ProjectForJavaRunner
 {
     public static void main(String[] args)
     {
-        Button on = new Button(true);
-        Button off = new Button(false);
-        
-        if(on.getPower())
-        {
-            System.out.println("on works");
-        }
-        if(!off.getPower())
-        {
-            System.out.println("off works");
-        }
-        System.out.println("ye");
+        gui game = new gui(800, 800);
     }
 }
